85c1e505e2c11fedfa1630f445111170a04aa4f7
chisssel <platov.artem.15@gmail.com>
2025-12-12 17:28:04 +0300

    Added styles

--- form/mainForm.py
+++ form/mainForm.py
@@ -1,15 +1,14 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-
 @app.route('/')
 def form():
     return render_template('form.html')
 
 @app.route('/greet', methods=['POST'])
 def greet():
-    name = request.form.get('name', 'Гость')
-    return f"<h1>Привет, {name}!</h1>"
+    name = request.form.get('name')
+    return render_template('greet.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
